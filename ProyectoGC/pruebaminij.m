9bdfcc13bf6c68e4ca9ed5a6cc81b732ccf6962e
panconar <patochiva@hotmail.com>
2015-10-26 11:27:43 -0600

    Update pruebaminij.m

--- ProyectoGC/pruebaminij.m
+++ ProyectoGC/pruebaminij.m
@@ -1,4 +1,17 @@
 % pruebaminij.m
+%Este script file sirve para resolver sistemas de ecuaciones de la forma:
+% A * x = b
+%Donde A es una matriz de minij
+%b es un vector de unos
+
+%Se encuentra la respuesta x por dos métodos diferentes, Gradiente conjugado y 
+%Gradiente conjugado Precondicionado.
+%El precondicionamiento se da de dos formas diferentes, una es usando una matriz
+%de Jacobi y el otro es usando Cholesky incompleto que proporciona matlab.
+
+%La salida es la impresión de el tipo de precondicionamiento que se uso, el tamaño de 
+%la matriz de minij, el número de iteraciones que tomo GCPre con ese precondicionamiento
+%y la norma del punto encontrado menos el encontrado por Gradiente Conjugado sin precondicionar
 S = char('Jacobi', 'Cholesky incompleto');
 n = 10:5:20;
 
